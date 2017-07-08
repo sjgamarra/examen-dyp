1677a75f963c3d0f478e16b053c7d50889b3afa6
Postgrado <Postgrado@F20LAB213-08E>
2017-07-08 14:03:10 -0500

    un mensajote

--- isw-amde-final/src/com/amde/linecredit/domain/services/LineCalculationDomainService.java
+++ isw-amde-final/src/com/amde/linecredit/domain/services/LineCalculationDomainService.java
@@ -9,10 +9,13 @@ import com.amde.linecredit.domain.model.Loan;
 public class LineCalculationDomainService {
 
 	public BigDecimal performLineCreditCalculation(Customer customer) throws Exception{
-		BigDecimal baseLineCredit = BigDecimal.ZERO;
-		//TODO; yada yada el codigo que hace la validacion entre las deudas del customer y genera una linea base de credito.
-		//TODO: lanzar excepciones de sobregiro, no deuda, peligroso etc.
-			
+		BigDecimal baseLineCredit = BigDecimal.ZERO;	
+		if(customer.canBeLoan()){
+			baseLineCredit = BigDecimal.valueOf(10000);
+			//TODO; yada yada el codigo que hace la validacion entre las deudas del customer y genera una linea base de credito.
+		}else{
+			throw new CantBeLoanException();
+		}
 		
 		return baseLineCredit;
 	}
